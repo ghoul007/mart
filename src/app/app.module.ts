4bb55563cb54e3aa7c3e80a7e93bc06112a4f28d
ahmed <ahmedgh@rosafian.com>
2018-09-27 23:31:22 +0100

    feat(routing): add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, ManagerModule, InventoryModule, PosModule, UserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/manager/manager.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManagerRoutingModule } from './manager-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ManagerRoutingModule
+  ],
+  declarations: []
+})
+export class ManagerModule { }
